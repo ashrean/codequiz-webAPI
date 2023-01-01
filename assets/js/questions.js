fb1c45761b83fcaf63d90d386ab5857807ec9d16
Ashley Sese <sese.ashrean@gmail.com>
2022-12-31 20:17:25 -0800

    need to finish question js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,74 @@
 var questions = [
     {
+        // question 1 //
         question: "Which one is a data type?",
         choices: ["alerts", "javascript", "strings", "var"],
         answer: "strings"
     },
 
     {
+        // question 2 //
         question: "One of these is not the 3 building blocks of the web...",
         choices: ["python", "css", "javascipt", "html"],
         answer: "python"
     },
 
     {
-        question: ""
-    }
+        // question 3 //
+        question: "The if/else statement is enclosed with _____",
+        choices: ["curly braces", "brackets", "quotes", "parantheses"],
+        answer: "parantheses"
+    },
+
+    {
+        // question 4 //
+        question:
+        choices:
+        answer:
+    },
+
+    {
+        // question 5 //
+        question:
+        choices:
+        answer:
+    },
+
+    {
+        // question 6 //
+        question:
+        choices:
+        answer:
+
+    },
+
+    {
+        // question 7 //
+        question:
+        choices:
+        answer:
+    },
+
+    {
+        // question 8 //
+        question:
+        choices:
+        answer:
+    },
+
+    {
+        // question 9 //
+        question:
+        choices:
+        answer:
+    },
+
+    {
+        // question 10 //
+        question:
+        choices:
+        answer:
+    },
+
+
 ]
