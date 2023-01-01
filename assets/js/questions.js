f7b9d91efe17fb2ed9378d59cb563312a5e465d1
Ashley Sese <sese.ashrean@gmail.com>
2022-12-31 18:17:32 -0800

    started questions on js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,17 @@
 var questions = [
+    {
+        question: "Which one is a data type?",
+        choices: ["alerts", "javascript", "strings", "var"],
+        answer: "strings"
+    },
 
-    
+    {
+        question: "One of these is not the 3 building blocks of the web...",
+        choices: ["python", "css", "javascipt", "html"],
+        answer: "python"
+    },
+
+    {
+        question: ""
+    }
 ]
