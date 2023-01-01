c70c99dfdd9608d711e0a19baa54d8bcbf520337
Ashley Sese <sese.ashrean@gmail.com>
2023-01-01 13:11:26 -0800

    created and half way done with quiz js

---
+++ assets/js/quiz.js
@@ -0,0 +1,14 @@
+// Welcome Page //
+// We are going to be using the const so values will not be reassigned //
+
+const welcomeElm = document.querySelector ("#welcome");
+const startQuizBtnElm = document.querySelector ("#startQuiz");
+
+// Quiz Elements //
+const quizElm = document.querySelector ("#quiz");
+const questionElm = docutment.querySelector ("#question");
+const answerElm = document.querySelector ("#answers");
+
+// Input Score Elements //
+const inputScoreElm = document.querySelector ("#inputScore");
+const initials
